9d1a2522cd1e8147d4508eaee61a8c3216b3bacc
Ray Tieu <ray.tieu@gmail.com>
2020-09-02 18:09:38 -0700

    added app.js file and schema

---
+++ app.js
@@ -0,0 +1,143 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "1234",
+  database: "employer_tracker_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  addViewUpdate();
+});
+
+function addViewUpdate() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "addViewUpdate",
+      message: "Please select one of the following options:'.",
+      choices: [
+        "Add departments, roles, employees",
+        "View departments, roles, employees",
+        "Update employee roles"
+      ]
+    }
+  ]).then(function(answer) {
+    const choice = answer.addViewUpdate;
+    if (choice === "Add departments, roles, employees") {
+      addEmployees();
+    }
+    else if (choice === "View departments, roles, employees") {
+      viewEmployees();
+    }
+    else if (choice === "Update employee roles") {
+      updateEmployees();
+    }
+  });
+}
+
+function addEmployees() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "firstName",
+      message: "Employee's first name:"
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: "Employee's last name"
+    },
+    {
+      type: "input",
+      name: "roleID",
+      message: "Role ID:"
+    },
+    {
+      type: "input",
+      name: "managerID",
+      message: "Manager's ID:"
+    }
+  ]).then(function(data) {
+    console.log("Inserting an employee...\n");
+    var query = connection.query(
+      "INSERT INTO employee SET ?",
+      {
+        first_name: data.firstName,
+        last_name: data.lastName,
+        role_id: data.roleID,
+        manager_id: data.managerID
+      },
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " employee inserted!\n");
+      }
+    );
+
+    console.log(query.sql);
+
+  });
+}
+  
+function viewEmployees() {
+  console.log("Selecting all employees...\n");
+  connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
+
+function updateEmployees() {
+  console.log("Updating employee...\n");
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "firstName",
+      message: "Employee's first name:"
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: "Employee's last name"
+    },
+    {
+      type: "input",
+      name: "roleID",
+      message: "Role ID:"
+    },
+    {
+      type: "input",
+      name: "managerID",
+      message: "Manager's ID:"
+    }
+  ]).then(function(data) {
+    console.log("Updating employee...\n");
+    var query = connection.query(
+      "UPDATE employee SET ? WHERE ?",
+      [
+        {
+          first_name: data.firstName,
+          last_name: data.lastName,
+          role_id: data.roleID,
+          manager_id: data.managerID
+        },
+        {
+          id: 
+        }
+      ],
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " employee updated!\n");
+      }
+    );
+  
+  console.log(query.sql);
+
+  });
+}
---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employer_tracker_db;
+
+CREATE DATABASE employer_tracker_db;
+
+USE employer_tracker_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT(10) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT(10) NOT NULL,
+  manager_id INT(10) NULL,
+  PRIMARY KEY (id)
+);
